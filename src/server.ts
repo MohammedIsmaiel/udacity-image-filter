c1a7449059867e17d261ad55b582aed13c238770
Mohammed_Ismaiel <mohammedismaiel333.z@gmail.com>
2021-07-23 17:57:20 +0200

    the filter route was added

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import { Request, Response } from "express";
+var validUrl = require('valid-url');
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 5000;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,20 +31,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    let img:String = req.query.image_url
+    if (!img) {return res.send("You must add an image_URL")}
+    
+    if (!validUrl.isUri(img)){return res.send("You must add a valid image_URL")}
+
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
-app.get('/*',(req,res)=>{
-  res.status(404).send('Not found')
-})
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
+
+  app.get("/*", (req, res) => {
+    res.status(404).send("Not found");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
+import { fromNode } from "bluebird";
