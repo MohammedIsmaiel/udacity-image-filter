bb7ad86fd5d472096493eb99f53c7df61832ee5d
Mohammed_Ismaiel <mohammedismaiel333.z@gmail.com>
2021-07-24 18:25:23 +0200

    comments was added

--- src/server.ts
+++ src/server.ts
@@ -15,24 +15,7 @@ import jwt from "jsonwebtoken";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  //auth
+  //auth method 
   const requireAuth = (req: Request, res: Response, next: NextFunction) =>{
     
     if (!req.headers || !req.headers.authorization){
@@ -47,7 +30,8 @@ import jwt from "jsonwebtoken";
       return next();
     });
 }
-  let images:any = [];
+
+//filtering route
   app.get("/filteredimage", requireAuth,async (req: Request, res: Response) => {
     let img:string = req.query.image_url;
     if (!img) {return res.send("You must add an image_URL")};
@@ -68,6 +52,7 @@ import jwt from "jsonwebtoken";
   });
 
   var token;
+  //get yor token route
   app.get("/get-access/:name", async (req: Request, res: Response) => {
     let name:string =  req.params.name;
     if (!name) {return res.send("you must provide a name (/get-access/your-name)")};
